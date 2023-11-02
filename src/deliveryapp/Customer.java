b2d8bb2beb0ade9254ffac805cdc07b0411b6aa6
Shazin <ahmedshazin21@gmail.com>
2023-11-02 11:46:26 +0800

    Implement Order class and add functions for customer class

--- src/deliveryapp/Customer.java
+++ src/deliveryapp/Customer.java
@@ -1,13 +1,23 @@
 package deliveryapp;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 public class Customer extends User {
-    private String cart, orderHistory;
+    private String location;
     private double balance;
+    private ArrayList<Integer> cart = new ArrayList<>(); // List of item IDs in the cart
+    private ArrayList<Order> orderHistory = new ArrayList<>(); // List of orders
+    private int customerId;
     
-    
-    public Customer(String username, String password, double balance) {
-        super(username, password, 2);
+    public Customer(String username, String password, int customerId, double balance) {
+        super(username, password, 2); // Assuming 2 represents the user type for customers
         this.balance = balance;
+        this.customerId = customerId;
+    }
+    
+    public int getCustomerId() {
+        return customerId;
     }
 
     public double getBalance() {
@@ -16,6 +26,48 @@ public class Customer extends User {
 
     public void setBalance(double balance) {
         this.balance = balance;
+    }
+    
+    public String viewReviews(int vendorId) {
+        // Implement this method to view reviews for a vendor
+        return "reviews";
+    }
+    
+    public String selectVendor(int vendorId) {
+        // Implement this method to select a vendor and view their menu
+        return "menu";
+    }
+    
+    public void addToCart(int itemId) {
+        cart.add(itemId);
+    }
+    
+    public void clearCart() {
+        cart.clear();
+    }
+    
+    public void setLocation(String location) {
+        this.location = location;
+    }
+     
+    public String getLocation() {
+        return location;
+    }
+    
+    public int placeOrder() {
+        int orderId = 0; //Generate an orderid, something like item ids + customer id + date and b64 encode? may change this to string
+        Order newOrder = new Order(orderId, getCustomerId(), location, new Date(), new ArrayList<>(cart));
+        orderHistory.add(newOrder);
+        cart.clear();
+        return orderId;
+    }
+    
+    public void cancelOrder(int orderId) {
+        // Implement this method to cancel a specific order
+    }
+    
+    public ArrayList<Order> viewOrders() {
+        return orderHistory;
     }
     
     
---
+++ src/deliveryapp/Order.java
@@ -0,0 +1,92 @@
+package deliveryapp;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Order {
+    
+    public enum OrderStatus {
+        PENDING,
+        IN_PROGRESS,
+        DELIVERED,
+        CANCELED
+    }
+    
+    private int orderId;
+    private int customerId;
+    private String deliveryLocation;
+    private Date orderDate;
+    private ArrayList<Integer> cart; // List of item IDs in the order
+    private double totalAmount;
+    private OrderStatus status;
+
+    public Order(int orderId, int customerId, String deliveryLocation, Date orderDate, ArrayList<Integer> cart) {
+        this.orderId = orderId;
+        this.customerId = customerId;
+        this.deliveryLocation = deliveryLocation;
+        this.orderDate = orderDate;
+        this.cart = cart;
+        this.totalAmount = calculateTotalAmount();
+        this.status = OrderStatus.PENDING; // Initialize as pending
+    }
+
+    // Getters and Setters for order attributes
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public String getDeliveryLocation() {
+        return deliveryLocation;
+    }
+
+    public Date getOrderDate() {
+        return orderDate;
+    }
+
+    public ArrayList<Integer> getCart() {
+        return cart;
+    }
+
+    public double getTotalAmount() {
+        return totalAmount;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    // Method to calculate the total price of the order
+    private double calculateTotalAmount() {
+        double total = 0.0;
+        // Calculate the total amount based on the item IDs in the cart
+        // Get price list from file/menu and sum up everything
+        for (int itemId : cart) {
+            //double itemPrice = getItemPriceFromDatabase(itemId);
+            //total += itemPrice;
+        }
+        return total;
+    }
+
+    // Get price of item
+    private double getItemPriceFromDatabase(int itemId) {
+        // Fetch the item price from file 
+        // Return the item price
+        return 10.0; // PLACEHOLDER
+    }
+
+    // Update order status
+    public void updateStatus(OrderStatus newStatus) {
+        status = newStatus;
+    }
+
+    // Cancel order
+    public void cancelOrder() {
+        status = OrderStatus.CANCELED;
+    }
+}
+
